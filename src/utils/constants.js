154049f12f386bd95be19d33d94cec4ff04e6b90
Anastasia Belova <nastasya.belowa@yandex.ru>
2023-10-28 18:41:32 +0300

    url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,9 @@ const REGEX_EMAIL = "[a-zA-Z0-9_.]+@[a-zA-Z0-9_]+\\.[a-z]{2,}";
 const REGEX_NAME = "^[A-Za-zА-Яа-я\\s\\-]{2,30}$";
 
 const MOVIE_URL = "https://api.nomoreparties.co";
-// const BASE_URL = "https://api.nastasya.nomoredomainsicu.ru";
+const BASE_URL = "https://api.nastasya.nomoredomainsicu.ru";
 
-const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3000";
 
 const SCREEN_LARGE = 1066;
 const SCREEN_MEDIUM = 678;
